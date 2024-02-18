385cbece07c574340cff92b05f685700961f09b0
ophicial-pauloski <pasukwo5@gmail.com>
2024-02-18 09:18:42 +0100

    Deployed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -37,8 +37,8 @@ const Header = () => {
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
-              ProShop
+              <img src={logo} alt='Buynow' />
+              Buynow
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
